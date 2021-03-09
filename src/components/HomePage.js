f0eee2b23208132ef6fb20893cd1871ef095812b
lucassherwin <lksherwin@gmail.com>
2021-03-09 11:06:51 -0500

    removed material ui styles and continued using styled components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { scroller } from "react-scroll";
+import { Button } from '@material-ui/core';
 
 const Home = styled.div`
   height: 100vh;
@@ -17,12 +18,12 @@ const Welcome = styled.div`
   padding: 10px
 `;
 
-const Button = styled.button`
-  font-size: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid red;
-  border-radius: 3px;
-`;
+// const Button = styled.button`
+//   font-size: 1em;
+//   padding: 0.25em 1em;
+//   border: 2px solid red;
+//   border-radius: 3px;
+// `;
 
 export default function HomePage() {
 
@@ -31,7 +32,7 @@ export default function HomePage() {
       <Welcome>
         <p>Hello, I'm Lucas Sherwin.</p>
         <p>I am a Full Stack Developer.</p>
-        <Button onClick={() => scroller.scrollTo('about', {smooth: true, duration: 1000})}>See my work</Button>
+        <Button variant='outlined' color='primary' onClick={() => scroller.scrollTo('about', {smooth: true, duration: 1000})}>See my work</Button>
       </Welcome>
     </Home>
   )
