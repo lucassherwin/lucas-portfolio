d0ff439670cc76e8d09f448e10c52107bc613dd1
lucassherwin <lksherwin@gmail.com>
2021-03-12 18:07:03 -0500

    added axios

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 export default function Blog() {
+
+  const mediumURL = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lksherwin";
+
   return (
     <div id='blog'>
       <h1>Blog Page</h1>
